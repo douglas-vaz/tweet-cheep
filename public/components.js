6a394343a04dd20a01f2b11072feb35cfff51c47
Douglas Vaz <douglas1992@gmail.com>
2014-07-23 20:09:34 +0530

    Added 'Clear' button to reset state. Updated browser support for transitions
    (Chrome support for transitions is horrible)

--- public/components.js
+++ public/components.js
@@ -10,45 +10,46 @@ var TweetList = React.createClass({
 	componentWillMount: function() {
 		var self = this;
 		socket.on('newTweet', function (tweet) {
-			console.log(tweet);
-
-			var tweets = self.state.data;
-			tweets.unshift(tweet);
-			self.setState({data: tweets});
+			if(!hide) {
+				var tweets = self.state.data;
+				if (tweets.length > 200) { tweets = [] };
+				tweets.unshift(tweet);
+				self.setState({data: tweets});
+			}
 
   		});
 	},
 	componentDidUpdate: function() {
 		// //Redirect hashtag clicks to input
-		// for (var ls = document.links, numLinks = ls.length, i=0; i<numLinks; i++){
-		// 	var link = ls[i].href;
-		// 	if(link.substring(0, 27) == 'https://twitter.com/hashtag'){
-		// 		console.log(link);
-		// 		console.log(link.substring(link.lastIndexOf('/')+1, link.lastIndexOf('?')));
-
-
-		// 		ls[i].onclick = function(e){
-		// 			e.preventDefault();
-  //  					inputTag.value = this.href.substring(this.href.lastIndexOf('/')+1, this.href.lastIndexOf('?'));
-		// 		};
-		// 	}
-		// }
+		for (var ls = document.links, numLinks = ls.length, i=0; i<numLinks; i++){
+			var link = ls[i].href;
+			if(link.substring(0, 27) == 'https://twitter.com/hashtag'){
+				ls[i].onclick = function(e){
+					e.preventDefault();
+   					inputTag.value = this.href.substring(this.href.lastIndexOf('/')+1, this.href.lastIndexOf('?'));
+				};
+			}
+		}
 	},
+	clearInput: function() {
+      // Clear the input
+      console.log("Clearing..");
+      this.replaceState(this.getInitialState());
+    },
 	render: function() {
 		  var tweetNodes = this.state.data.map(function (tweet) {
-		  console.log(tweet.key);
 	      return (
-	      	<ReactCSSTransitionGroup transitionName="tweet">
 	        	<Tweet key={tweet.key} rendered={tweet.rendered}>
 	        	</Tweet>
-        	</ReactCSSTransitionGroup>
-
 	      );
 	    });
 
 		return (
-			<div className="tweetList">
+			<div className="tweetList" ref="incomingTweets">
+			<button className="clearButton" onClick={this.clearInput}>Clear</button>
+	      	<ReactCSSTransitionGroup transitionName="tweet">
 			{tweetNodes}
+        	</ReactCSSTransitionGroup>
 			</div>
 		);
 	}
