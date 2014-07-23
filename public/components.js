44b8ec6130804ad313b8bf0f0957b9cf51fca215
Douglas Vaz <douglas1992@gmail.com>
2014-07-23 12:33:44 +0530

    Fallback fixes

--- public/components.js
+++ public/components.js
@@ -19,26 +19,25 @@ var TweetList = React.createClass({
   		});
 	},
 	componentDidUpdate: function() {
-		//Redirect hashtag clicks to input
-		for (var ls = document.links, numLinks = ls.length, i=0; i<numLinks; i++){
-			var link = ls[i].href;
-			if(link.substring(0, 27) == 'https://twitter.com/hashtag'){
-				console.log(link);
-				console.log(link.substring(link.lastIndexOf('/')+1, link.lastIndexOf('?')));
+		// //Redirect hashtag clicks to input
+		// for (var ls = document.links, numLinks = ls.length, i=0; i<numLinks; i++){
+		// 	var link = ls[i].href;
+		// 	if(link.substring(0, 27) == 'https://twitter.com/hashtag'){
+		// 		console.log(link);
+		// 		console.log(link.substring(link.lastIndexOf('/')+1, link.lastIndexOf('?')));
 
 
-				ls[i].onclick = function(e){
-					e.preventDefault();
-   					inputTag.value = this.href.substring(this.href.lastIndexOf('/')+1, this.href.lastIndexOf('?'));
-				};
-			}
-		}
+		// 		ls[i].onclick = function(e){
+		// 			e.preventDefault();
+  //  					inputTag.value = this.href.substring(this.href.lastIndexOf('/')+1, this.href.lastIndexOf('?'));
+		// 		};
+		// 	}
+		// }
 	},
 	render: function() {
 		  var tweetNodes = this.state.data.map(function (tweet) {
 		  console.log(tweet.key);
 	      return (
-	      	<button onClick={this.replaceState(this.getInitialState())}>Clear</button>
 	      	<ReactCSSTransitionGroup transitionName="tweet">
 	        	<Tweet key={tweet.key} rendered={tweet.rendered}>
 	        	</Tweet>
